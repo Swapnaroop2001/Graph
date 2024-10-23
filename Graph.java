5208c0fc0b6bc299e3ca4b4caf16f457acea8a29
Swapnaroop Salgare <swapnaroop2001@gmail.com>
2024-10-23 18:19:36 -0400

    Clone graph

--- Graph.java
+++ Graph.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.List;
+import java.util.HashMap;
 
 /**
  * Graph
@@ -109,6 +111,46 @@ public class Graph {
 
         return area;
     }
+
+    //Clone Graph
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    private HashMap<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node==null){
+            return null;
+        }
+
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val);
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return cloneNode;
+    }
+
+
+   
 
     public static void main(String[] args) {
         int V = 5;
